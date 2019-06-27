1dc0bc96ae382972cbe83536d9c48e2c277c16bd
榊原昌彦 <sakakibara@rdlabo.jp>
2019-06-27 20:58:21 +0900

    WIP: showBanner

--- ios/Plugin/Plugin/Plugin.m
+++ ios/Plugin/Plugin/Plugin.m
@@ -4,5 +4,6 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(AdMob, "AdMob",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(initialize, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(showBanner, CAPPluginReturnPromise);
 )
--- ios/Plugin/Plugin/Plugin.swift
+++ ios/Plugin/Plugin/Plugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import GoogleMobileAds
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -7,11 +8,44 @@ import Capacitor
  */
 @objc(AdMob)
 public class AdMob: CAPPlugin {
-    
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
+
+    var bannerView: GADBannerView!
+    var bannerUIView: UIViewController!
+
+    @objc func initialize(_ call: CAPPluginCall) {
+        let appId = call.getString("appId") ?? "ca-app-pub-6564742920318187~7217030993"
         call.success([
-            "value": value
-        ])
+            "value": appId
+            ])
+    }
+
+    @objc func showBanner(_ call: CAPPluginCall) {
+        DispatchQueue.main.async {
+            self.bannerView = GADBannerView(adSize: kGADAdSizeBanner)
+            self.bannerView.translatesAutoresizingMaskIntoConstraints = false
+            self.bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+            self.bannerView.backgroundColor = UIColor(red: 10, green: 0, blue: 0, alpha: 100)
+
+            if let rootViewController = UIApplication.shared.keyWindow?.rootViewController {
+                rootViewController.view.addSubview(self.bannerView)
+                rootViewController.view.addConstraints(
+                    [NSLayoutConstraint(item: self.bannerView,
+                                        attribute: .bottom,
+                                        relatedBy: .equal,
+                                        toItem: self.bannerView,
+                                        attribute: .top,
+                                        multiplier: 1,
+                                        constant: 0),
+                     NSLayoutConstraint(item: self.bannerView,
+                                        attribute: .centerX,
+                                        relatedBy: .equal,
+                                        toItem: rootViewController.view,
+                                        attribute: .centerX,
+                                        multiplier: 1,
+                                        constant: 0)
+                    ])
+                self.bannerView.load(GADRequest())
+            }
+        }
     }
 }
