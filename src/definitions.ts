751dd1c07ab29f7221d68e785575b4d2fc50e7ae
rdlabo <sakakibara@rdlabo.jp>
2021-02-18 01:32:27 +0900

    update(): change definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -142,7 +142,7 @@ export interface AdMobPlugin {
    */
   addListener(
     eventName: 'onAdOpened',
-    listenerFunc: (info: void) => void,
+    listenerFunc: () => void,
   ): PluginListenerHandle;
 
   /**
@@ -155,7 +155,7 @@ export interface AdMobPlugin {
    */
   addListener(
     eventName: 'onAdClosed',
-    listenerFunc: (info: void) => void,
+    listenerFunc: () => void,
   ): PluginListenerHandle;
 
   /**
@@ -168,7 +168,7 @@ export interface AdMobPlugin {
    */
   addListener(
     eventName: 'adViewDidReceiveAd',
-    listenerFunc: (info: void) => void,
+    listenerFunc: () => void,
   ): PluginListenerHandle;
 
   /**
@@ -511,10 +511,30 @@ export interface AdMobRewardItem {
   amount: number;
 }
 
+/**
+ * For more information
+ * https://developers.google.com/android/reference/com/google/android/gms/ads/AdError
+ */
 export interface AdMobError {
+  /**
+   * Gets the error's code.
+   */
   code: number;
+
+  /**
+   * Gets the message describing the error.
+   */
   message: string;
+
+  /**
+   * Gets the cause of this error or null if the cause is nonexistent or unknown.
+   */
   cause: string;
+
+  /**
+   * Gets the domain of the error.
+   * MobileAds.ERROR_DOMAIN for Google Mobile Ads SDK errors, or a domain defined by mediation networks for mediation errors.
+   */
   domain: string;
 }
 
