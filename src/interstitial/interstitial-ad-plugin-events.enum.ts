e145d2235f2f464866e10bc6836e1a88607fbc5a
Saninn Salas Diaz <5490201+distante@users.noreply.github.com>
2021-04-23 10:08:13 +0200

    fix: interstitial even names fix

--- src/interstitial/interstitial-ad-plugin-events.enum.ts
+++ src/interstitial/interstitial-ad-plugin-events.enum.ts
@@ -3,18 +3,18 @@ export enum InterstitialAdPluginEvents {
   /**
    * Emits after trying to prepare and Interstitial, when it is loaded and ready to be show
    */
-  Loaded = 'onRewardedVideoAdLoaded',
+  Loaded = 'interstitialAdLoaded',
   /**
    * Emits after trying to prepare and Interstitial, when it could not be loaded
    */
-  FailedToLoad = 'onRewardedVideoAdFailedToLoad',
+  FailedToLoad = 'interstitialAdFailedToLoad',
   /**
    * Emits when the Interstitial ad is visible to the user
    */
-  Showed = 'onRewardedVideoAdShowed',
-  FailedToShow = 'onRewardedVideoAdFailedToShow',
+  Showed = 'interstitialAdShowed',
+  FailedToShow = 'interstitialAdFailedToShow',
   /**
    * Emits when the Interstitial ad is not visible to the user anymore.
    */
-  Dismissed= 'onRewardedVideoAdDismissed',
+  Dismissed= 'interstitialAdDismissed',
 }
