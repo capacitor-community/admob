4254a43ba8083b5a8854f8c0be36d642901a1471
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2022-01-09 16:48:33 +0900

    chore(ng): update to 13

--- demo/angular/src/app/app.module.ts
+++ demo/angular/src/app/app.module.ts
@@ -11,13 +11,12 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
