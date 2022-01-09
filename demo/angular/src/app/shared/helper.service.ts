0ea91bfe8d0c45e92145757d8895339ba6daf9fd
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2022-01-09 17:56:34 +0900

    chore

--- demo/angular/src/app/shared/helper.service.ts
+++ demo/angular/src/app/shared/helper.service.ts
@@ -1,21 +1,25 @@
 import { Injectable, NgZone } from '@angular/core';
-import {ITestItems} from './interfaces';
-import {BannerAdPluginEvents} from '../../../../../dist/esm/banner';
+import { ITestItems } from './interfaces';
+import { BannerAdPluginEvents } from '../../../../../dist/esm';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HelperService {
-
-  constructor(private zone: NgZone) { }
+  constructor(private zone: NgZone) {}
 
   /**
    * items is not Deep Copy, this is substitution
    */
-  public async updateItem(items: ITestItems[], name: string, result: boolean, value: any = undefined) {
+  public async updateItem(
+    items: ITestItems[],
+    name: string,
+    result: boolean,
+    value: any = undefined,
+  ) {
     this.zone.run(() => {
       let isChanged = false;
-      items = items.map((item) => {
+      items = items.map(item => {
         if (item.name === name && item.result === undefined && !isChanged) {
           isChanged = true;
           if (item.expect === undefined) {
@@ -25,27 +29,33 @@ export class HelperService {
             item.result = item.expect.includes(value.toString());
           } else {
             if (item.name === BannerAdPluginEvents.SizeChanged) {
-              item.result = this.bannerAdPluginEventsSizeChanged(item.expect as number, value);
+              item.result = this.bannerAdPluginEventsSizeChanged(
+                item.expect as number,
+                value,
+              );
             } else if (item.expect === 'error') {
-              item.result = this.receiveErrorValue(value)
+              item.result = this.receiveErrorValue(value);
             }
           }
         }
         return item;
       });
     });
-    await new Promise(resolve => setTimeout(() => resolve(), 1000));
+    await new Promise<void>(resolve => setTimeout(() => resolve(), 1000));
   }
 
-  private bannerAdPluginEventsSizeChanged(expect: number | string, value: any): boolean {
+  private bannerAdPluginEventsSizeChanged(
+    expect: number | string,
+    value: any,
+  ): boolean {
     if (expect === 0) {
       return value.width === 0 && value.height === 0;
     }
-    return value.width > 0 && value.height > 0
+    return value.width > 0 && value.height > 0;
   }
 
   private receiveErrorValue(value: any): boolean {
-    console.log(['これ', value])
-    return value.hasOwnProperty('code') &&  value.hasOwnProperty('message');
+    console.log(['これ', value]);
+    return value.hasOwnProperty('code') && value.hasOwnProperty('message');
   }
 }
