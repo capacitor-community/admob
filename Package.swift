852f415d4e7f6727b20b51d21e06b63f9c171642
榊原昌彦 <sakakibara@rdlabo.jp>
2024-11-27 15:51:30 +0900

    fix(): add forgetting comma

--- Package.swift
+++ Package.swift
@@ -10,16 +10,16 @@ let package = Package(
             targets: ["AdMobPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main")
-            .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.12.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", branch: "main")
     ],
     targets: [
         .target(
             name: "AdMobPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
-                    .product(name: "AdMob", package: "swift-package-manager-google-mobile-ads")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
             ],
             path: "ios/Sources/AdMobPlugin"),
         .testTarget(
