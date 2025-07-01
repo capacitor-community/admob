670dd691ee7772c373a91b63e62be4d6d1273268
Diogo Nunes <d13nunes@gmail.com>
2025-07-01 11:30:25 +0100

    chore: support admob 12.6.0 ios

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.3.0")
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "12.6.0")
     ],
     targets: [
         .target(
