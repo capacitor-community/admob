2032c4d5e6828d2fc50d928e7710953afed0846a
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2026-09-11 10:37:33 +0900

    chore: update demo to Capacitor 8.5 and SPM (#462)

--- demo/angular/ios/App/App/AppDelegate.swift
+++ demo/angular/ios/App/App/AppDelegate.swift
@@ -11,6 +11,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let config = UISceneConfiguration(name: "Default Configuration",
+                                          sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        return config
+    }
+
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
