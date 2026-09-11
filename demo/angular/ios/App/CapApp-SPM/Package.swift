5eb26e944be8378590aed94f1d965ad65bc22e52
rdlabo <sakakibara@rdlabo.jp>
2026-09-11 10:22:05 +0900

    chore: update demo to Capacitor 8.5 and SPM

--- demo/angular/ios/App/CapApp-SPM/Package.swift
+++ demo/angular/ios/App/CapApp-SPM/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.4.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.5.1"),
         .package(name: "CapacitorCommunityAdmob", path: "../../../../..")
     ],
     targets: [
